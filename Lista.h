ad45eb417e773e9f2a598e8279f60baccddfde0d
LucasHenriqueP <lucashenrique.pereira@hotmail.com>
2018-03-29 16:19:31 -0300

    Add files via upload

---
+++ Lista.h
@@ -0,0 +1,55 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+# include <iostream>
+# define iMaxListSize 32
+
+typedef int T;
+
+
+class Lista
+{
+    public:
+        Lista(void);
+        virtual ~Lista();
+        // Metodos Acessores
+        //Recupera o n�mero de objetos da lista
+        int getSize(void) const;
+        //Recupera a informa��o contida em uma determinada posi��o
+        T getData(int, T&) const;
+
+        //M�todos Modificadores
+        //Altera o conteudo da lista em uma determinada posi��o
+        int setData(int, T&);
+        //Insere um objeto no final da lista
+        int insert(const T&);
+        //Remove a primeira ocorr�ncia de um objeto da lista
+        int remove(const T&);
+        //Remove o primeiro objeto da lista
+        int removeFirst(T&);
+        //Remove todos os objetos da lista
+        void removeAll(void);
+        //Outros m�todos
+        //Retorna um valor booleano caso a lista n�o seja vazia
+        bool isEmpty(void) const;
+        //Encontra a primeira ocorr�ncia de um objeto da lista
+        bool find(T&) const ;
+        // imprime o conteudo da lista
+        void print() const;
+
+        //ordenacao pelo m�todo bubbleSort
+        void bubbleSort();
+        // ordenacao pelo metodo selectionSort
+        void selectionSort();
+
+    protected:
+
+    private:
+        //Vetor que ir� conter os objetos da lista
+        int listData[ iMaxListSize];
+        //N�mero de objetos da lista
+        int iSize;
+
+};
+
+#endif // LISTA_H
